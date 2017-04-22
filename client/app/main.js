9fd20f3d3216a4c63190b6d6e694c46931a4af45
Erfan Ebrahimnia <eebrahimnia.mmt-b2015@fh-salzburg.ac.at>
2017-04-23 00:32:35 +0200

    added base setup including webpack build, react, node server and socketio

---
+++ client/app/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HelloWorldComponent } from 'components/hello-world';
+
+
+ReactDOM.render(<HelloWorldComponent />, document.getElementById('main'));
