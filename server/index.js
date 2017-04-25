fef74e3c4f9e27235931955e22e80b0ab01df099
Christoph Karl Mayr <cmayr.mmt-b2015@fh-salzburg.ac.at>
2017-04-25 16:53:45 +0200

    updated call of socket

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const config = require('config');
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const socket = require('./socket'); 
+const socket = require('./socket');
 
 const rootPath = path.join(__dirname, config.get('http.rootPath'));
 
@@ -13,6 +13,6 @@ app.use(express.static(rootPath)); // set the root path to our client folder
 
 http.listen(process.env.PORT || config.get('http.port'));
 
-socket(http, config);
+socket(http);
 
 console.log('Server listening on port: ', process.env.PORT || config.get('http.port'));
