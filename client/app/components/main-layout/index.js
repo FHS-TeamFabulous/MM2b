76b09752871a32d226a6e8fb453fb11b5d216f6b
Christoph Karl Mayr <cmayr.mmt-b2015@fh-salzburg.ac.at>
2017-04-25 16:58:54 +0200

    implemented videocomponent

--- client/app/components/main-layout/index.js
+++ client/app/components/main-layout/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import VideoComponent from '../client/video';
 
-export default function(props) {
+export default function MainLayout (props) {
     return (
         <div>
             { props.children }
+            <VideoComponent name="christoph"/>
+            <VideoComponent name="erfan"/>
         </div>
     );
 }
