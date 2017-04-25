c4ffb0854495b8fcddaf230f1ef3744d120a52f5
Christoph Karl Mayr <cmayr.mmt-b2015@fh-salzburg.ac.at>
2017-04-25 16:56:11 +0200

    implemented signaling

--- server/socket.js
+++ server/socket.js
@@ -1,154 +1,238 @@
-var socketIO = require('socket.io'),
-    uuid = require('node-uuid'),
-    crypto = require('crypto');
+const socketIO = require('socket.io'),
+    users = {};
 
-module.exports = function (server, config) {
+function sendTo(conn) {
+    return (message) => {
+        conn.emit('message', message);
+    };
+
+}
+
+const connectionTypes = {
+    LOGIN: 'login',
+    LOGOUT: 'logout',
+    OFFER: 'offer',
+    ANSWER: 'answer',
+    CANDIDATE: 'candidate',
+};
+
+module.exports = function (server) {
     const io =socketIO(server);
 
-    io.on('connection', function (client) {
-        client.resources = {
-            screen: false,
-            video: true,
-            audio: false
-        };
+    const extractData = message => {
+        let data;
+        try {
+            data = JSON.parse(message);
+        } catch (e) {
+            console.log('Invalid JSON message');
+            data = {};
+        }
+        return data;
+    };
 
-        console.log('client connected: ', client.id);
+    /*const isValidMessage = (message, condition = true) => message.name && condition;
+    const isOfferMessage = message => isValidMessage(message, message.offer);
+    const isAnswerMessage = message => isValidMessage(message, message.answer);
+    const isCandidateMessage = message => isValidMessage(message, message.candidate);*/
 
+    io.on('connection', connection => {
+
+        console.log('connected: ' + connection.id);
+        console.log('clients logged in: ', Object.keys(users));
+        const clientSender = sendTo(connection);
+
+        clientSender({
+            type: 'clients',
+            clients: Object.keys(users)
+        });
         // pass a message to another id
-        client.on('message', function (details) {
-            console.log('message', details);
-            if (!details) return;
+        connection.on('message', data => {
 
-            var otherClient = io.to(details.to);
-            if (!otherClient) return;
+            const handleLogin = data => {
+                const {
+                    name
+                } = data;
 
-            details.from = client.id;
-            otherClient.emit('message', details);
+                console.log(`User logging in ${name}`);
+
+                if (users[name]) {
+                    console.log(`${name} already logged in`);
+                    clientSender( {
+                        type: connectionTypes.LOGIN,
+                        success: false
+                    });
+                } else {
+                    connection.resources = {
+                        screen: false,
+                        video: true,
+                        audio: false
+                    };
+                    connection.name = name;
+                    users[name] = connection;
+
+                    clientSender({
+                        type: connectionTypes.LOGIN,
+                        success: true,
+                        clients: Object.keys(users)
+                    });
+                }
+            };
+
+            const handleLogout = data => {
+                const {
+                    name
+                } = data;
+
+                console.log("Disconnecting from", name);
+
+                const conn = users[name];
+                conn.otherName = null;
+
+                delete users[name];
+
+                //notify the other user so he can disconnect his peer connection
+                if (conn != null) {
+                    clientSender({
+                        type: connectionTypes.LOGOUT,
+                        success: true
+                    });
+                } else {
+                    clientSender({
+                        type: connectionTypes.LOGOUT,
+                        success: false
+                    });
+                }
+            };
+
+            const handleOffer = data => {
+                const {
+                    name,
+                    offer
+                } = data;
+
+                console.log("Sending offer to: ", name);
+
+                //if UserB exists then send him offer details
+                const conn = users[name];
+
+                if (conn != null) {
+                    //setting that UserA connected with UserB
+                    connection.otherName = name;
+
+                    sendTo(conn)({
+                        type: connectionTypes.OFFER,
+                        offer,
+                        name: connection.name
+                    });
+                } else {
+                    clientSender({
+                        type: 'error',
+                        message: `[OFFER] Connection ${name} not found`
+                    })
+                }
+
+            };
+
+            const handleAnswer = data => {
+                const {
+                    name,
+                    answer
+                } = data;
+
+                console.log("Sending answer to: ", data.name);
+
+                //for ex. UserB answers UserA
+                const conn = users[name];
+
+                if (conn != null) {
+                    connection.otherName = name;
+                    sendTo(conn)({
+                        type: connectionTypes.ANSWER,
+                        answer
+                    });
+                } else {
+                    clientSender({
+                        type: 'error',
+                        message: `[ANSWER] Connection ${name} not found`
+                    });
+                }
+            };
+
+            const handleCandidate = data => {
+                const {
+                    name,
+                    candidate
+                } = data;
+
+                console.log("Sending candidate to:", data.name);
+
+                const conn = users[name];
+
+                if (conn != null) {
+                    sendTo(conn)({
+                        type: connectionTypes.CANDIDATE,
+                        candidate
+                    });
+                } else {
+                    clientSender({
+                        type: 'error',
+                        message: `[CANDIDATE] Connection ${name} not found`
+                    });
+                }
+            };
+
+            switch (data.type) {
+                case connectionTypes.LOGIN:
+                    handleLogin(data);
+                    break;
+                case connectionTypes.LOGOUT:
+                    handleLogout(data);
+                    break;
+                case connectionTypes.OFFER:
+                    handleOffer(data);
+                    break;
+                case connectionTypes.ANSWER:
+                    handleAnswer(data);
+                    break;
+                case connectionTypes.CANDIDATE:
+                    handleCandidate(data);
+                    break;
+                default:
+                    clientSender({type: 'error', message: `Invalid message type (${data.type}`})
+            }
         });
 
-        client.on('shareScreen', function () {
-            client.resources.screen = true;
+        connection.on('shareScreen', function () {
+            connection.resources.screen = true;
         });
 
-        client.on('unshareScreen', function (type) {
-            client.resources.screen = false;
+        connection.on('unshareScreen', function (type) {
+            connection.resources.screen = false;
             removeFeed('screen');
         });
 
-        client.on('join', join);
+        connection.on("disconnect", function() {
 
-        function removeFeed(type) {
-            if (client.room) {
-                io.in(client.room).emit('remove', {
-                    id: client.id,
-                    type: type
-                });
-                if (!type) {
-                    client.leave(client.room);
-                    client.room = undefined;
+            console.log('disconnect');
+            if(connection.name) {
+                console.log(`Disconnecting ${connection.name}`);
+                delete users[connection.name];
+
+                if(connection.otherName) {
+                    console.log("Disconnecting from ", connection.otherName);
+                    var conn = users[connection.otherName];
+                    conn.otherName = null;
+
+                    if(conn != null) {
+                        sendTo(conn, {
+                            type: "leave"
+                        });
+                    }
                 }
             }
-        }
 
-        function join(name, cb) {
-            // sanity check
-            if (typeof name !== 'string') return;
-            // check if maximum number of clients reached
-            if (config.rooms && config.rooms.maxClients > 0 &&
-                clientsInRoom(name) >= config.rooms.maxClients) {
-                safeCb(cb)('full');
-                return;
-            }
-            // leave any existing rooms
-            removeFeed();
-            safeCb(cb)(null, describeRoom(name));
-            client.join(name);
-            client.room = name;
-        }
-
-        // we don't want to pass "leave" directly because the
-        // event type string of "socket end" gets passed too.
-        client.on('disconnect', function () {
-            removeFeed();
+            console.log(Object.keys(users));
         });
-        client.on('leave', function () {
-            removeFeed();
-        });
-
-        client.on('create', function (name, cb) {
-            if (arguments.length == 2) {
-                cb = (typeof cb == 'function') ? cb : function () {};
-                name = name || uuid();
-            } else {
-                cb = name;
-                name = uuid();
-            }
-            // check if exists
-            var room = io.nsps['/'].adapter.rooms[name];
-            if (room && room.length) {
-                safeCb(cb)('taken');
-            } else {
-                join(name);
-                safeCb(cb)(null, name);
-            }
-        });
-
-        // support for logging full webrtc traces to stdout
-        // useful for large-scale error monitoring
-        client.on('trace', function (data) {
-            console.log('trace', JSON.stringify(
-            [data.type, data.session, data.prefix, data.peer, data.time, data.value]
-            ));
-        });
-
-
-        // tell client about stun and turn servers and generate nonces
-        client.emit('stunservers', config.stunservers || []);
-
-        // create shared secret nonces for TURN authentication
-        // the process is described in draft-uberti-behave-turn-rest
-        var credentials = [];
-        // allow selectively vending turn credentials based on origin.
-        var origin = client.handshake.headers.origin;
-        if (!config.turnorigins || config.turnorigins.indexOf(origin) !== -1) {
-            config.turnservers.forEach(function (server) {
-                var hmac = crypto.createHmac('sha1', server.secret);
-                // default to 86400 seconds timeout unless specified
-                var username = Math.floor(new Date().getTime() / 1000) + (server.expiry || 86400) + "";
-                hmac.update(username);
-                credentials.push({
-                    username: username,
-                    credential: hmac.digest('base64'),
-                    urls: server.urls || server.url
-                });
-            });
-        }
-        client.emit('turnservers', credentials);
     });
-
-
-    function describeRoom(name) {
-        var adapter = io.nsps['/'].adapter;
-        var clients = adapter.rooms[name] || {};
-        var result = {
-            clients: {}
-        };
-        Object.keys(clients).forEach(function (id) {
-            result.clients[id] = adapter.nsp.connected[id].resources;
-        });
-        return result;
-    }
-
-    function clientsInRoom(name) {
-        return io.sockets.clients(name).length;
-    }
-
 };
 
-function safeCb(cb) {
-    if (typeof cb === 'function') {
-        return cb;
-    } else {
-        return function () {};
-    }
-}
