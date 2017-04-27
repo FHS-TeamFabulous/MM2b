7019dfcc2d594baf7789f79e860929836ad812e4
Erfan Ebrahimnia <eebrahimnia.mmt-b2015@fh-salzburg.ac.at>
2017-04-27 18:19:38 +0200

    I want to commit suicide

--- server/index.js
+++ server/index.js
@@ -18,6 +18,10 @@ app.use(cors());
 app.use('/api/', apiRoutes);
 app.use('/assets/', express.static(assetsPath));
 
+app.get('/', function(req, res) {
+    res.sendFile('./index.html');
+});
+
 http.listen(process.env.PORT || config.get('http.port'));
 
 socket(http, config);
