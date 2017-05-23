038a6f98c4b5a1b31bcc0f9cf026f24d9895c503
Christoph Karl Mayr <cmayr.mmt-b2015@fh-salzburg.ac.at>
2017-05-23 16:16:30 +0200

    added redirect to https on heroku

--- server/index.js
+++ server/index.js
@@ -8,12 +8,14 @@ const app = express();
 const http = require('http').Server(app);
 const socket = require('./socket');
 const apiRoutes = require('./api-routes')(express.Router());
+const sslRedirect = require('heroku-ssl-redirect');
 
 const clientPath = path.join(__dirname, config.get('paths.client'));
 const assetsPath = path.join(__dirname, config.get('paths.assets'));
 
 // middleware
 app.use(express.static(clientPath)); // set the root path to our client folder
+app.use(sslRedirect());
 app.use(cors());
 app.use('/api/', apiRoutes);
 app.use('/assets/', express.static(assetsPath));
